f65453a7cb31f540c1dd76d84de12f4a515389a8
Nishith18 <87228052+Nishith18@users.noreply.github.com>
2024-09-13 01:31:56 +0530

    Add files via upload

---
+++ Number Guessing Game.py
@@ -0,0 +1,41 @@
+import random
+
+top_of_range = input("Enter the number range: ")
+
+if top_of_range.isdigit():
+    top_of_range=int(top_of_range)
+
+    if top_of_range<=0:
+        print("Please print a number larger than 0 next time")
+        quit()
+
+else:
+    print("Please print a digit next time")
+    quit()
+
+random_number=random.randrange(0,top_of_range)
+
+guesses=0
+while True:
+    guesses+=1
+    user_guess = input("Guess the number: ")
+
+    if user_guess.isdigit():
+        user_guess=int(user_guess)
+    else:
+        print("Please print a digit next time")
+        continue
+
+    if user_guess==random_number:
+        print("You guessed it right!")
+        break
+    elif user_guess>random_number:
+        print("You were above the number")
+    else:
+        print("You were below the number")
+
+
+
+print("You got it in",guesses, "guess"+("es" if guesses!=1 else ""))
+
+
